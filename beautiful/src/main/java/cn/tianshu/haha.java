4945ba86937ad5f7cfdd2aac9b298ba3dc087709
1234 <1234@163.com>
2018-10-29 18:24:45 +0800

    git test

---
+++ beautiful/src/main/java/cn/tianshu/haha.java
@@ -0,0 +1,17 @@
+package cn.tianshu;
+
+import org.junit.Test;
+
+public class haha {
+    private String y="you";
+    private String a="are";
+
+    public static void main(String[] args) {
+ //       System.out.println(y+a+"beautiful");
+    }
+
+    @Test
+    public void Testt(){
+        System.out.println(y+"\t"+a+"\t"+"beautiful");
+    }
+}
